5dd513adc7753bece8cd4cb28ee609afa107db05
Thoriq Fathurrozi <thoriqfathurrozi@gmail.com>
2025-05-23 23:55:12 +0700

    fix: change user seeder credential and name

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -22,8 +22,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin RT',
+            'email' => 'admin@mail.com',
         ]);
 
         $this->call([
